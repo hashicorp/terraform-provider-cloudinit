35a97325a3f77f74666ce51fd277a3338d882f37
Benjamin Bennett <ben.bennett@hashicorp.com>
2023-06-02 16:55:42 +0100

    Add copywrite files

--- tools/tools.go
+++ tools/tools.go
@@ -1,9 +1,12 @@
-//go:build tools
-// +build tools
+//go:build generate
 
 package tools
 
 import (
 	// document generation
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+	// copywrite header generation
+	_ "github.com/hashicorp/copywrite"
 )
+
+//go:generate go run github.com/hashicorp/copywrite headers -d .. --config ../.copywrite.hcl
